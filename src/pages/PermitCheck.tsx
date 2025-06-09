20243664310b92327d03a0fc7a4d48a09d6434f6
Brian <brian.wahinya.njau@huawei.com>
2025-06-09 22:13:09 +0300

    Chore remove unused libraries

--- src/pages/PermitCheck.tsx
+++ src/pages/PermitCheck.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const PermitCheck = () => {
   return <div>PermitCheck</div>;
 };
--- src/pages/PostCode.tsx
+++ src/pages/PostCode.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const PostCode = () => {
   return <div>PostCode</div>;
 };
